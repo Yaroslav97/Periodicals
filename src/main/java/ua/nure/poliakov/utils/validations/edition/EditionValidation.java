4f1f82692862de208c62ab16f5bf8b17734b6dde
Yaroslav97 <poliakov.yaroslav@gmail.com>
2017-01-21 13:02:49 +0200

    improve some func

--- src/main/java/ua/nure/poliakov/utils/validations/edition/EditionValidation.java
+++ src/main/java/ua/nure/poliakov/utils/validations/edition/EditionValidation.java
@@ -4,6 +4,8 @@ import javax.xml.bind.ValidationException;
 
 public interface EditionValidation<T, D> {
     boolean name(T name) throws ValidationException;
+
     boolean subject(T subject) throws ValidationException;
+
     boolean price(D price) throws ValidationException;
 }
