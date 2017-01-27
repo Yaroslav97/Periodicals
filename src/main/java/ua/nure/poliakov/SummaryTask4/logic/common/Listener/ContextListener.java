82cfff20bd44f4070e9a884031c4058108872896
Yaroslav97 <poliakov.yaroslav@gmail.com>
2017-01-27 12:01:34 +0200

    improve code

--- src/main/java/ua/nure/poliakov/SummaryTask4/logic/common/Listener/ContextListener.java
+++ src/main/java/ua/nure/poliakov/SummaryTask4/logic/common/Listener/ContextListener.java
@@ -1,7 +1,5 @@
 package ua.nure.poliakov.SummaryTask4.logic.common.listener;
 
-import org.apache.log4j.Logger;
-
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -9,15 +7,17 @@ import javax.servlet.annotation.WebListener;
 @WebListener
 public class ContextListener implements ServletContextListener {
 
-    private static final Logger log = Logger.getLogger(ContextListener.class);
-
     @Override
     public void contextInitialized(ServletContextEvent sce) {
-        log.info("Start Periodicals");
+        log("Start Periodicals");
+    }
+
+    private void log(String msg) {
+        System.out.println("Application: " + msg);
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
-        log.info("Destroy Periodicals");
+        log("Periodicals finished");
     }
 }
